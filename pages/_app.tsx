003c4b6cde7aa64c5a8eeff86c6757b523df9a6d
Marissa Wagner <marissawagner@Marissas-MacBook-Pro.local>
2024-01-26 15:30:41 -0500

    refactor _app.tsx to wrap content in a div instead of fragment, add tw classes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,10 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Component {...pageProps} />
       <Analytics />
       <SpeedInsights />
-    </>
+    </div>
   );
 }
